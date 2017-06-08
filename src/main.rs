7f1b34c03d2f91d1575cf607387b1042419e7228
Avi Weinstock <aweinstock314@gmail.com>
2017-06-08 15:54:20 -0400

    Implement partial derivative solver.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::ops::{Add, Mul};
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Expr<T, U> {
     Variable(T),
     Constant(U),
@@ -29,8 +29,24 @@ impl<T, U> Expr<T, U> {
     }
 }
 
+fn differentiate(e: Expr<char, u32>, var: char) -> Expr<char, u32> {
+    match e {
+        Expr::Variable(x) if x == var => Expr::Constant(1),
+        Expr::Variable(_) => Expr::Constant(0),
+        Expr::Constant(_) => Expr::Constant(0),
+        Expr::Plus(e1, e2) => differentiate(*e1, var) + differentiate(*e2, var),
+        Expr::Times(e1,e2) => *e1.clone() * differentiate(*e2.clone(), var) + *e2 * differentiate(*e1, var),
+        _ => unimplemented!(),
+    }
+}
 
 fn main() {
     use Expr::*;
     println!("{:?}", (Variable("x") + Constant(42)).pow(Constant(2) * Variable("y")));
+    let tmp = Variable('x') + Variable('x') + Constant(2);
+    println!("Before: {:?}", tmp);
+    println!("After: {:?}", differentiate(tmp, 'x'));
+    let tmp = Variable('x') * Variable('x') + Constant(2);
+    println!("Before: {:?}", tmp);
+    println!("After: {:?}", differentiate(tmp, 'x'));
 }
