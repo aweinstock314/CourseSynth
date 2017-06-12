8c2df25e0d142c604e45d75fc335eaee73cd15f9
Avi Weinstock <aweinstock314@gmail.com>
2017-06-12 15:37:44 -0400

    Implemented simplification for expressions.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+#![feature(box_patterns)]
 extern crate futures;
 extern crate hyper;
+extern crate num_traits;
 
 use futures::{Future, IntoFuture};
 use hyper::server::{Http, Service, Request, Response};
 use hyper::{Get, StatusCode};
+use num_traits::*;
+use std::fmt::Debug;
 use std::net::ToSocketAddrs;
 use std::ops::{Add, Mul};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 enum Expr<T, U> {
     Variable(T),
     Constant(U),
@@ -47,6 +51,40 @@ fn differentiate(e: Expr<char, u32>, var: char) -> Expr<char, u32> {
     }
 }
 
+fn simplify<T, U: Eq+Zero+One>(e: Expr<T, U>) -> Expr<T, U> {
+    use Expr::*;
+    match e {
+        // Constant evalutation
+        Plus(box Constant(x), box Constant(y)) => Constant(x+y),
+        Times(box Constant(x), box Constant(y)) => Constant(x*y),
+
+        // Algebraic identities
+        Plus(box Constant(x), box y) => if x == zero() { y } else { Constant(x) + y },
+        Plus(box x, box Constant(y)) => if y == zero() { x } else { x + Constant(y) },
+        Times(box Constant(x), box y) => if x == one() { y } else { Constant(x) * y },
+        Times(box x, box Constant(y)) => if y == one() { x } else { x * Constant(y) },
+
+        // Recursion into subtrees
+        Plus(box x, box y) => { simplify(x) + simplify(y) }
+        Times(box x, box y) => { simplify(x) * simplify(y) }
+        Power(box x, box y) => { simplify(x).pow(simplify(y)) }
+        x => x,
+    }
+}
+
+fn converge<T: Debug+Eq, F: Fn(&T) -> T>(f: F, x: T) -> T {
+    let mut last = x;
+    let mut i = 0;
+    loop {
+        println!("converge, i={}, last={:?}", i, last); i += 1;
+        let next = f(&last);
+        if next == last {
+            return next;
+        }
+        last = next;
+    }
+}
+
 struct Website;
 
 impl Service for Website {
@@ -63,15 +101,19 @@ impl Service for Website {
     }
 }
 
+fn test1(e: Expr<char, u32>) {
+    println!("e: {:?}", e);
+    let e1 = differentiate(e, 'x');
+    println!("differentiated: {:?}", e1);
+    let e2 = converge(|x| simplify(x.clone()), e1);
+    println!("simplfied: {:?}", e2);
+}
+
 fn main() {
     use Expr::*;
     println!("{:?}", (Variable("x") + Constant(42)).pow(Constant(2) * Variable("y")));
-    let tmp = Variable('x') + Variable('x') + Constant(2);
-    println!("Before: {:?}", tmp);
-    println!("After: {:?}", differentiate(tmp, 'x'));
-    let tmp = Variable('x') * Variable('x') + Constant(2);
-    println!("Before: {:?}", tmp);
-    println!("After: {:?}", differentiate(tmp, 'x'));
+    test1(Variable('x') + Variable('x') + Constant(2));
+    test1(Variable('x') * Variable('x') + Constant(2));
 
     let addr = ("127.0.0.1", 8000).to_socket_addrs().unwrap().next().unwrap();
     Http::new().bind(&addr, || Ok(Website)).expect("Failed to initialize http server.").run().expect("An error occurred while running the server.");
